792319ac3108abc054d1c14f3121303885d78878
Ngarnica <aligarnica88@gmail.com>
2022-11-18 19:25:01 -0500

    Se crea servicio de envío de emails

--- servidor.py
+++ servidor.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+import json
 
 print(__name__)
 
@@ -45,30 +46,43 @@ def registro():
 @application.route('/enviarMail', methods =['POST'])
 def enviarMail():
 
-    message = Mail(
-                from_email= os.environ.get('SENDGRID_SENDER_DRONENET'),
-                to_emails='mapagusu@gmail.com',
-                subject='Email enviado desde python DroneNet',
-                html_content='<strong>Probando el envio de emails desde Python</strong>')
-    try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY_DRONENET'))
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+    tipoContenido = request.content_type
 
-        return {
+    if( request.is_json ):
+        datos = request.json
+
+    
+        
+        try:
+            message = Mail(
+                from_email= os.environ.get('SENDGRID_SENDER_DRONENET'),
+                to_emails= datos ["to"] ,
+                subject= datos ["subject"],
+                html_content= datos["message"])
+                
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY_DRONENET'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+
+            return {
             "message" : "El email se envio con exito",
             "code" : response.status_code
-        }
+            }
 
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
-        return {
+            return {
             "message" : "No se pudo enviar el email",
             "error" : e
-        }
+            }
+    else:
+        return {
+            "message" : "No es posible procesar su solicitud",
+            "error" : 404
+            }
 
 if  __name__  ==  '__main__':
      application.run(host='127.0.0.1',  port=8000, debug=True)
